0bc18083d2f8aab60b736d74453faac60f2cac0e
guillermo <gugomea@unirioja.es>
2022-12-07 00:00:33 +0100

    paint en local

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Paint paint_local = new Paint();
+        paint_local.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.out.println("Paint se ha cerrado");
+                System.exit(0);
+            }
+        });
     }
 }
